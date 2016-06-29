f70b9d42215453b77fde953fe3b9cfa14d85b0c4
mcolula <m.colula@gmail.com>
2016-06-29 10:36:21 -0500

    Problem 10081 solved!

---
+++ DPlevel2/10081-TightWords.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+const int A = 101;
+const int B =  11;
+int N, K, T =   0;
+double test[A][B];
+double memo[A][B];
+
+double probability(int idx, int num) {
+  if (idx == N)
+    return 100;
+  if (test[idx][num] == T)
+    return memo[idx][num];
+  double ans = probability(idx + 1, num) / (K + 1.0);
+  if (num + 1 <= K)
+    ans += probability(idx + 1, num + 1) / (K + 1.0);
+  if (num - 1 >= 0)
+    ans += probability(idx + 1, num - 1) / (K + 1.0);
+  test[idx][num] = T;
+  return memo[idx][num] = ans;
+}
+
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  cout <<  fixed  <<  setprecision(5);
+  while (T++, cin >> K >> N) {
+    double ans = 0.0;
+    for (int i = 0; i <= K; i++)
+      ans += probability(1, i) / (K + 1.0);
+    cout << ans << "\n";
+  }
+  return 0;
+}
